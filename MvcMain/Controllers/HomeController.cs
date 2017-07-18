005830b47fd1b1444250fc5f37a0046c89f4c658
ali2 <ali62n62@yahoo.com>
2017-07-18 12:35:44 +0430

    wcf service removed

--- MvcMain/Controllers/HomeController.cs
+++ MvcMain/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-//using s1;
+using S1;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,13 +14,6 @@ namespace MvcMain.Controllers
         // GET: /<controller>/
         public async Task<IActionResult> Index()
         {
-            //Service1Client service1 =
-            //    new Service1Client(Service1Client.EndpointConfiguration.BasicHttpBinding_IService1);
-            //string hi=await service1.SayHelloAsync();
-            
-            //service1.CloseAsync();
-            //ViewBag.Hi = hi;
-
             return View();
         }
     }
