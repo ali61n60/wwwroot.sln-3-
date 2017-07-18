4721fbe7ae58ce79b45c286b89e19745bd8d0960
ali2 <ali62n62@yahoo.com>
2017-07-18 16:05:45 +0430

    working with Configure Method

--- MvcMain/Startup.cs
+++ MvcMain/Startup.cs
@@ -22,6 +22,7 @@ namespace MvcMain
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
