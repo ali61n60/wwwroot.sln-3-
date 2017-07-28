f96c8a86a5f581512986ad8fc2dc462457e7abe4
Ali <ali62n62@yahoo.com>
2017-07-28 19:25:24 +0430

    authentication

--- MvcMain/Controllers/HomeController.cs
+++ MvcMain/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
 
 namespace MvcMain.Controllers
 {
