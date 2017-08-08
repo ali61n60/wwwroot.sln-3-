2be5f798703cb355349fe8a5284ee3ff24dfcf5d
ali2 <ali62n62@yahoo.com>
2017-08-08 17:35:41 +0430

    working on login page

--- MvcMain/Controllers/AccountController.cs
+++ MvcMain/Controllers/AccountController.cs
@@ -2,12 +2,124 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MvcMain.Models;
 
 namespace MvcMain.Controllers
 {
     public class AccountController:Controller
     {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+        private ConfigurationRoot _appConfiguration;
+
+        public AccountController(UserManager<AppUser> userMgr, SignInManager<AppUser> signinMgr)
+        {
+            _userManager = userMgr;
+            _signInManager = signinMgr;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result =
+                        await _signInManager.PasswordSignInAsync(user, details.Password, details.RememberMe, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email),
+                    "Invalid user or password");
+            }
+            return View(details);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        public IActionResult PasswordForget(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PasswordForget(PasswordForgetModel detail, string returnUrl)
+        {
+            //TODO Email user password or do appropriate action
+            return RedirectToAction("Login",new { returnUrl =returnUrl?? "/" });
+        }
+
+        [AllowAnonymous]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result
+                    = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToRoute("/");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+
+
+
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
 
     }
 }
---
+++ MvcMain/Models/UserViewModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace MvcMain.Models
+{
+    public class CreateModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+        
+        [Required]
+        [UIHint("remember me")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class PasswordForgetModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+    }
+
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+    }
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
