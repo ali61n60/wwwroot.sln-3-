c123585a2a4c1f2c422be29543e1ed2803d6dbab
ali2 <ali62n62@yahoo.com>
2017-08-08 11:15:33 +0430

    add Identity

--- MvcMain/Startup.cs
+++ MvcMain/Startup.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using MvcMain.Models;
 
 namespace MvcMain
 {
@@ -24,6 +31,34 @@ namespace MvcMain
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(_configuration["Data:ConnectionString"]));
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+                {
+                    options.Password.RequireUppercase = false;
+                    options.User.RequireUniqueEmail = true;
+                    options.Cookies.ApplicationCookie.Events =
+                        new CookieAuthenticationEvents
+                        {
+                            OnRedirectToLogin = context =>
+                            {
+                                if (context.Request.Path.StartsWithSegments("/api") &&
+                                    context.Response.StatusCode == (int)HttpStatusCode.OK)
+                                {
+                                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                                    context.Response.ContentType = "application/json";
+                                    context.Response.WriteAsync("\"What did you say?\"");
+                                }
+                                else
+                                    context.Response.Redirect(context.RedirectUri);
+                                return Task.CompletedTask;
+                            }
+                        };
+                }
+                ).AddEntityFrameworkStores<AppIdentityDbContext>();
+
+
             services.AddMvc();
         }
 
@@ -32,11 +67,24 @@ namespace MvcMain
         {
             app.UseStatusCodePages();
             app.UseStaticFiles();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtBearer:Key"])),
+                    ValidAudience = _configuration["JwtBearer:SiteUrl"],
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = _configuration["JwtBearer:SiteUrl"]
+                }
+            });
+            app.UseIdentity();
             app.UseMvcWithDefaultRoute();
         }
     }
