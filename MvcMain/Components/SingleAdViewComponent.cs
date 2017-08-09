5e533f3070b26b8b2ffcf79d37ec9b3ee4d740d9
ali2 <ali62n62@yahoo.com>
2017-08-09 10:40:51 +0430

    search page

---
+++ MvcMain/Components/SingleAdViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMain.Components
+{
+    public class SingleAdViewComponent:ViewComponent
+    {
+        public SingleAdViewComponent()
+        {
+            
+        }
+
+       
+
+        public IViewComponentResult Invoke()
+        {
+            //ViewBag.SelectedCategory = RouteData?.Values["category"];
+            //return View(repository.GetProducts()
+            //    .Select(x => x.Category)
+            //    .Distinct()
+            //    .OrderBy(x => x));
+            return null;
+        }
+    }
+}
