06fc620573ee7201c4bdebc3081b547baed51070
ali2 <ali62n62@yahoo.com>
2017-08-09 14:31:29 +0430

    modelstd added

---
+++ MvcMain/Controllers/AdApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcMain.Infrastructure;
+
+namespace MvcMain.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class AdApiController:Controller
+    {
+        [HttpGet]
+        public JsonResult SayHello([FromQuery][FromBody] string name,int numberOfCalls)
+        { 
+            return Json(String.Format("Hello {0},Number is {1} current server time is: {2}", name,numberOfCalls, DateTime.Now.ToString()));
+        }
+
+        public ResponseBase<AdvertisementCommon[]> GetAdvertisementCommon(int startIndex, int count, Dictionary<string, string> userInput)
+        {
+            startIndex = _serviceCommonPolicy.getStart(startIndex);
+            count = _serviceCommonPolicy.getCount(count);
+            int selectedCategoryId = getSelectedCategoryId(userInput);
+            //Polymorphic dispatch of service call
+            IAdvertisementService advertisementService = AdServiceDictionary.GetAdvertisementService(selectedCategoryId);
+            _response = advertisementService.GetAdvertisements(startIndex, count, userInput);
+            setRequestIndex(userInput);
+            return _response;
+        }
+
+    }
+}
---
+++ MvcMain/Infrastructure/ResponseBase.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMain.Infrastructure
+{
+    
+    public class ResponseBase<T>
+    {
+         public T ResponseData;
+        
+        public bool Success { get; set; }
+        
+        public string Message { get; set; }
+        
+        public string ErrorCode { get; set; }
+        
+        public Dictionary<string, string> CustomDictionary { get; set; }
+
+        public void SetSuccessResponse()
+        {
+            SetSuccessResponse("OK");
+        }
+
+        public void SetSuccessResponse(string message)
+        {
+            Success = true;
+            Message = message;
+        }
+
+        public void SetFailureResponse(string message)
+        {
+            Success = false;
+            Message = message;
+        }
+
+        public void SetFailureResponse(string message, string errorCode)
+        {
+            Success = false;
+            Message = message;
+            ErrorCode = errorCode;
+        }
+
+        public ResponseBase GetSimpleResponseBase()
+        {
+            ResponseBase responseBase = new ResponseBase();
+            responseBase.Success = this.Success;
+            responseBase.Message = this.Message;
+            responseBase.ErrorCode = this.ErrorCode;
+            return responseBase;
+        }
+    }
+
+    public class ResponseBase
+    {
+        
+        public bool Success { get; set; }
+        
+        public string Message { get; set; }
+
+        
+        public string ErrorCode { get; set; }
+
+        public void SetSuccessResponse()
+        {
+            SetSuccessResponse("OK");
+        }
+
+        public void SetSuccessResponse(string message)
+        {
+            Success = true;
+            Message = message;
+        }
+
+        public void SetFailureResponse(string message)
+        {
+            Success = false;
+            Message = message;
+        }
+
+        public void SetFailureResponse(string message, string errorCode)
+        {
+            Success = false;
+            Message = message;
+            ErrorCode = errorCode;
+        }
+    }
+}
