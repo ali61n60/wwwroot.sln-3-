efe76094f311f1060588fd9c819245224bbe7c1d
ali2 <ali62n62@yahoo.com>
2017-08-15 19:29:53 +0430

    quite done entity framework start

--- RepositoryStd/DB/ChikojaDbContext.cs
+++ RepositoryStd/DB/ChikojaDbContext.cs
@@ -4,17 +4,33 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace RepositoryStd.DB
 {
-    
+
+    public class TemporaryDbContextFactory : IDbContextFactory<ChikojaDbContext>
+    {
+        public ChikojaDbContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<ChikojaDbContext>();
+            builder.UseSqlServer(
+                "Data Source= .\\;Initial Catalog=ayoobfar_db;Persist Security Info=True;User ID=ayoobfar_ali;Password=119801;MultipleActiveResultSets=true");
+            //builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=pinchdb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new ChikojaDbContext(builder.Options);
+        }
+    }
+
+
+
+
     public class ChikojaDbContext:DbContext
     {
 
         public ChikojaDbContext(DbContextOptions options):base(options)
         {
-          
         }
+      
         public DbSet<Price> Prices { get; set; }
     }
 
--- TestEntity/Form1.cs
+++ TestEntity/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.EntityFrameworkCore;
+using RepositoryStd.DB;
 
 namespace TestEntity
 {
@@ -19,6 +21,21 @@ namespace TestEntity
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                TemporaryDbContextFactory temporaryDbContextFactory = new TemporaryDbContextFactory();
+                ChikojaDbContext dbContext = temporaryDbContextFactory.Create(null);
+                IQueryable<Price> allPrices = dbContext.Prices.Where(p => p.price1 > 0);
+                foreach (Price currentPrice in allPrices)
+                {
+                    listBox1.Items.Add((currentPrice.price1 + " ," + currentPrice.priceType));
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(ex.Message);
+            }
+           
 
         }
     }
