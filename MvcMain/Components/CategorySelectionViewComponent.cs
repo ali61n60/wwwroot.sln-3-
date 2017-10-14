71c74ede6e56abe4f27de6781c6925348bc3768c
Ali <ali62n62@yahoo.com>
2017-10-14 03:40:32 +0330

    category component created

---
+++ MvcMain/Components/CategorySelectionViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMain.Components
+{
+    public class CategorySelectionViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
