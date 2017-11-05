930b8a99a013756c3a74554b6183b50d9355ac85
ali2 <ali62n62@yahoo.com>
2017-11-05 16:16:30 +0330

    types done

---
+++ MvcMain/wwwroot/js/TypeScript/TypesDemo/anyDemo.ts
@@ -0,0 +1,9 @@
+﻿function anyDemo() {
+    let foo;
+    foo = 21;
+    console.log(`foo=${foo}`);
+    foo = "hello";
+    console.log(`foo=${foo}`);
+}
+
+anyDemo();
---
+++ MvcMain/wwwroot/js/TypeScript/TypesDemo/enumDemo.ts
@@ -0,0 +1,16 @@
+﻿function enumDemo() {
+    enum  Temperature {
+        Cold,
+        Hot
+    };
+
+    let temp = Temperature.Cold;
+
+    if (temp == Temperature.Cold) {
+        console.log("Brrr...");
+    }else if (temp == Temperature.Hot) {
+        console.log("yikes!!");
+    }
+}
+
+enumDemo();
