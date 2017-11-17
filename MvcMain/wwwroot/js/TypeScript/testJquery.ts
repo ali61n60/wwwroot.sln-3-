7120e2d3411af96cbc9740c3496fc70d9074d5f3
Ali <ali62n62@yahoo.com>
2017-11-17 06:00:49 +0330

    nodejs installed

--- MvcMain/wwwroot/js/TypeScript/testJquery.ts
+++ MvcMain/wwwroot/js/TypeScript/testJquery.ts
@@ -1,2 +1,13 @@
-﻿class MyTestClss {
+﻿// <reference path ="../../typings/globals/jquery/index.d.ts"/>
+
+class MyTestClss {
+    test(): void {
+        alert("$ in jquery.d.ts is working");
+    }
 }
+
+let myObject = new MyTestClss();
+$(document).ready(() => {
+    myObject.test();
+});
+
