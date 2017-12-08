86fced6eabd9beb48814f150d568e26a6676f3b3
Ali <ali62n62@yahoo.com>
2017-12-08 17:10:28 +0330

    working on temperature monitor

---
+++ ModelStd/Db/Ad/TemperatueModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelStd.Db.Ad
+{
+    [Table("Temperatures", Schema = "ad")]
+    public class TemperatueModel
+    {
+        [Key]
+        [Column("temperatureId")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TemperatureId { get; set; }
+
+        [Column("temp")]
+        public int Temp { get; set; }
+
+        [Column("insertDateTime", TypeName = "smalldatetime")]
+        public DateTime InsertDateTime { get; set; }
+    }
+}
--- RepositoryStd/Context/Ad/AdDbContext.cs
+++ RepositoryStd/Context/Ad/AdDbContext.cs
@@ -41,6 +41,7 @@ namespace RepositoryStd.Context.AD
         public virtual DbSet<Provinces> Provinces { get; set; }
         public virtual DbSet<SimilarAds> SimilarAds { get; set; }
         public virtual DbSet<Sms> Sms { get; set; }
+        public virtual DbSet<TemperatueModel> TemperatueModels { get; set; }
 
        
 
