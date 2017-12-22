c3e6baa1f78847c61ba836780a8ca6bb74f9c9c3
Ali <ali62n62@yahoo.com>
2017-12-22 14:41:34 +0330

    home started

--- MvcMain/wwwroot/js/Components/CategorySelection.js
+++ MvcMain/wwwroot/js/Components/CategorySelection.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var EventDispatcher_1 = require("../Events/EventDispatcher");
-var CategorySelection = (function () {
+var CategorySelection = /** @class */ (function () {
     function CategorySelection(parentDivId, allCategories) {
         this._firstLevelTemplate = "category1Template";
         this._firstLevelDiv = "category1";
