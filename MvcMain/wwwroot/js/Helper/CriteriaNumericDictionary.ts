7cf040c3dd7d51e148a1dc419900aacf8f47030d
ali2 <ali62n62@yahoo.com>
2018-01-06 11:11:05 +0330

    why not working

--- MvcMain/wwwroot/js/Helper/CriteriaNumericDictionary.ts
+++ MvcMain/wwwroot/js/Helper/CriteriaNumericDictionary.ts
@@ -1,6 +1,7 @@
 ﻿import { ICriteria} from "./ICriteria";
 import { NumericDictionary } from "lodash/dist/lodash";
 
+
 export class CriteriaNumericDictionary implements NumericDictionary<ICriteria> {
     [index: number]: ICriteria;
 }
