fd4dc29ec953bffbfb300020062c13be921bffee
ali2 <ali62n62@yahoo.com>
2018-01-08 15:41:49 +0330

    canvasJs added

--- MvcMain/Controllers/TemperatureController.cs
+++ MvcMain/Controllers/TemperatureController.cs
@@ -9,6 +9,7 @@ using RepositoryStd.TepmeratureRepository;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
+//https://canvasjs.com/
 namespace MvcMain.Controllers
 {
     public class TemperatureController : Controller
