a73509d37ad068d3e4bd3b06e8bfdfb480f6a893
Ali <ali62n62@yahoo.com>
2018-01-13 02:41:04 +0330

    done at home

--- MvcMain/Controllers/AdminController.cs
+++ MvcMain/Controllers/AdminController.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMain.Controllers
 {
-    //TODO manage submitted ads
+    //TODO copy role management pages from AnamSerkan Project
+    //TODO manage submitted ads, inform users registerd in LetMeKnow page
+    //TODO
     //TODO manage Ad image folder
     public class AdminController:Controller
     {
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Index()
         {
             return View();
--- MvcMain/Controllers/LetMeKnow.cs
+++ MvcMain/Controllers/LetMeKnow.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMain.Controllers
 {
+    //TODO enable users to register for items to be informed when a new ad meets user requirements
     public class LetMeKnow:Controller
     {
         [Authorize]
