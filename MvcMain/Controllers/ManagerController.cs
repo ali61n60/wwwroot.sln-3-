fad8d5792bb331209e2d6b5f3cb59290f08f73bb
ali2 <ali62n62@yahoo.com>
2018-01-13 12:57:45 +0330

    amdin and roleAdmin controllers added

---
+++ MvcMain/Controllers/ManagerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMain.Controllers
+{
+    //TODO copy role management pages from AnamSerkan Project
+    //TODO manage submitted ads, inform users registerd in LetMeKnow page
+    //TODO
+    //TODO manage Ad image folder
+    public class ManagerController:Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
