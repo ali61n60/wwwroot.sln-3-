b7683cf239af0be0cc2701ae59878595d4984859
ali2 <ali62n62@yahoo.com>
2018-01-16 14:50:13 +0330

    work on image

--- MvcMain/wwwroot/js/home/adDetail/src/adDetail.js
+++ MvcMain/wwwroot/js/home/adDetail/src/adDetail.js
@@ -2,7 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var AdDetail = (function () {
     function AdDetail() {
-        alert("AdDetail");
+        $("#imageGallery").lightSlider({
+            gallery: true,
+            item: 1,
+            loop: true,
+            thumbItem: 9,
+            slideMargin: 0,
+            enableDrag: false,
+            currentPagerPosition: 'left',
+            onSliderLoad: function (el) {
+                el.lightGallery({
+                    selector: '#imageGallery .lslide'
+                });
+            }
+        });
     }
     return AdDetail;
 }());
