0bf16633eb6a29d8e54dc265e91dfeb0b2653af0
Ali <ali62n62@yahoo.com>
2018-02-03 22:54:54 +0330

    ok

--- MvcMain/wwwroot/js/TypeScript/TypesDemo/anyDemo.js
+++
@@ -1,9 +0,0 @@
-function anyDemo() {
-    var foo;
-    foo = 21;
-    console.log("foo=" + foo);
-    foo = "hello";
-    console.log("foo=" + foo);
-}
-anyDemo();
-//# sourceMappingURL=anyDemo.js.map
