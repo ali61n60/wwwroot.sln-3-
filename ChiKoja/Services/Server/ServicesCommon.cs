d847a0c5ff5439f404060179bfde83fb67e646e5
ali2 <ali62n62@yahoo.com>
2018-02-05 15:03:14 +0330

    ok

--- ChiKoja/Services/Server/ServicesCommon.cs
+++ ChiKoja/Services/Server/ServicesCommon.cs
@@ -10,8 +10,8 @@ namespace ChiKoja.Services.Server
 {
     public class ServicesCommon
     {
-        //public static string ServerUrl = "http://192.168.42.76";
-        public static string ServerUrl = "http://192.168.43.19";
+        public static string ServerUrl = "http://192.168.42.76";
+        //public static string ServerUrl = "http://192.168.43.19";
 
         public static async Task<ResponseBase<T>> CallService<T>(string controllerActionUrlPart)
         {
