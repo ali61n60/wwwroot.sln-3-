d5c695d1e804b7e02f2987fb2b3019dcd00d1284
Ali <ali62n62@yahoo.com>
2018-02-07 03:54:54 +0330

    ok

--- ChiKoja/SearchAd/SearchAdFragment.cs
+++ ChiKoja/SearchAd/SearchAdFragment.cs
@@ -7,33 +7,29 @@ using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
 using ChiKoja.Categories;
-using ChiKoja.CustomViews.SingleAdView;
 using ChiKoja.Infrastructure.IOC;
 using ChiKoja.Models;
 using ChiKoja.Notification;
 using ChiKoja.Services.Server.Interfaces;
 using ChiKoja.SingleAds;
 using ModelStd.Advertisements;
-using ModelStd.Advertisements.Price;
 using ModelStd.Services;
 
 namespace ChiKoja.SearchAd
 {
-    public class SearchAdFragment:Fragment
+
+    //TODO handle click event of listViewAdCommon
+    //TODO remove unused ViewGroupSingleAd
+    public class SearchAdFragment : Fragment
     {
         private SingleAdArrayAdapter _singleAdArrayAdapter;
         private Context _context;
         View rootView;
-        
         Button buttonFilter;
-
         Button buttonSort;
         AppCompatButton buttonSearchAd;
-
         Button buttonCategory;
-        //LinearLayout searchResultPlaceHolder;
         private ListView listViewAdCommon;
-
         private CategorySelection _categorySelection;
         IAdApi _adApi;
 
@@ -41,14 +37,14 @@ namespace ChiKoja.SearchAd
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            rootView= inflater.Inflate(Resource.Layout.serarch_fragment, container, false);
-            
+            rootView = inflater.Inflate(Resource.Layout.serarch_fragment, container, false);
+
             initializeFields();
             initializeEvents();
 
             return rootView;
         }
-        
+
         public override void OnAttach(Context context)
         {
             base.OnAttach(context);
@@ -57,34 +53,24 @@ namespace ChiKoja.SearchAd
 
         private void initializeFields()
         {
-            _singleAdArrayAdapter=new SingleAdArrayAdapter(_context,
+            _singleAdArrayAdapter = new SingleAdArrayAdapter(_context,
                 Android.Resource.Layout.SimpleListItem1,
-                new List<AdvertisementCommon>()
-                {
-                    new AdvertisementCommon()
-                    {
-                        AdvertisementTitle = "hello",
-                        NumberOfVisit = 10,
-                        AdvertisementPrice = new AgreementPrice(),
-                        AdvertisementImages = new string[10]
-                    }
-                });
-            
+                new List<AdvertisementCommon>());
+
             listViewAdCommon = rootView.FindViewById<ListView>(Resource.Id.listViewAdCommon);
-         listViewAdCommon.Adapter = _singleAdArrayAdapter;
-           
-                
-                _adApi = Bootstrapper.container.GetInstance<IAdApi>();
+            listViewAdCommon.Adapter = _singleAdArrayAdapter;
+
+            _adApi = Bootstrapper.container.GetInstance<IAdApi>();
             buttonSearchAd = rootView.FindViewById<AppCompatButton>(Resource.Id.buttonSearch);
             buttonFilter = rootView.FindViewById<Button>(Resource.Id.buttonFilter);
             buttonSort = rootView.FindViewById<Button>(Resource.Id.buttonSort);
             buttonCategory = rootView.FindViewById<Button>(Resource.Id.buttonCategory);
             //searchResultPlaceHolder = rootView.FindViewById<LinearLayout>(Resource.Id.layoutSearchAdLinearLayout);
-            
+
             _categorySelection = new CategorySelection();
         }
 
-       private void initializeEvents()
+        private void initializeEvents()
         {
             buttonSearchAd.Click += async (sender, args) =>
             {
@@ -104,13 +90,12 @@ namespace ChiKoja.SearchAd
 
             _categorySelection.SelectedCategoryCahnged += (sender, args) =>
             {
-             
+
             };
         }
 
         void buttonCategory_Click(object sender, EventArgs e)
         {
-
             Intent categorySelectionIntent = new Intent(_context, typeof(ActivityCategory));
 
             categorySelectionIntent.PutExtra("CategorySelection", _categorySelection);
@@ -121,7 +106,6 @@ namespace ChiKoja.SearchAd
         {
             Intent OrderByIntent = new Intent(_context, typeof(ActivitySortBy));
             StartActivityForResult(OrderByIntent, NavigationDrawer.NavActivity.OrderByRequestCode);
-            
         }
 
         void buttonFilter_Click(object sender, EventArgs e)
@@ -132,47 +116,18 @@ namespace ChiKoja.SearchAd
 
         public void resetSearchCondition()
         {
-            //searchResultPlaceHolder.RemoveAllViews();
-           listViewAdCommon.RemoveAllViews();
+            //TODO maybe you should remove items from array adapter
+            listViewAdCommon.RemoveAllViews();
             _adApi.ResetSearchCondition();
-        }
-        public void addAdvertisementOnPage(AdvertisementCommon advertisementCommon, LinearLayout.LayoutParams layoutParams)
-        {
-            ViewGroupSingleAd viewGroupSingleAd = cretateNewViewSingleAd(advertisementCommon);
-            //searchResultPlaceHolder.AddView(viewGroupSingleAd, layoutParams);
-           
-        }
-
-        private ViewGroupSingleAd cretateNewViewSingleAd(AdvertisementCommon advertisementCommon)
-        {
-            return new ViewGroupSingleAd(_context)
-            {
-                AdTitle = advertisementCommon.AdvertisementTitle + ", " + advertisementCommon.CityName,
-                AdPrice = advertisementCommon.AdvertisementPrice.PriceString,
-                AdImage = advertisementCommon.AdvertisementImages[0],
-                AdCategoryId = advertisementCommon.AdvertisementCategoryId,
-                AdGuid = advertisementCommon.AdvertisementId,
-                AdNumberOfVisit = advertisementCommon.NumberOfVisit
-            };
         }
 
         public void OnSerachAdCompleted(ResponseBase<AdvertisementCommon[]> response)
         {
             GlobalApplication.GlobalApplication.GetMessageShower().ShowDefaultMessage();
-            
-
-            LinearLayout.LayoutParams layoutParams =
-                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
-            layoutParams.SetMargins(0, 20, 0, 20);
-            //TODO use list instead 
             if (response.Success)
             {
                 _singleAdArrayAdapter.AddItemsToList(response.ResponseData);
                 ((SingleAdArrayAdapter)listViewAdCommon.Adapter).NotifyDataSetChanged();
-                // listViewAdCommon.InvalidateViews();
-               
-                //foreach (AdvertisementCommon advertisementCommon in response.ResponseData)
-                //  addAdvertisementOnPage(advertisementCommon, layoutParams);
             }
             else
                 Toast.MakeText(_context, response.Message, ToastLength.Long).Show();
