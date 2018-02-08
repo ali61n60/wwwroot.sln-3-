7c31e5770b3a052bf102c37cec6b98be2bfed19a
Ali <ali62n62@yahoo.com>
2018-02-08 15:26:59 +0330

    android project changed

--- ChiKoja/AdDetail/AdDetailActivity.cs
+++ ChiKoja/AdDetail/AdDetailActivity.cs
@@ -22,6 +22,8 @@ namespace ChiKoja.AdDetail
 
         private void inflateView()
         {
+
+            //TODO get adguid and category id from extra and based on that load category specific view in the fragment
             FrameLayout contentFrameLayout =
                 FindViewById<FrameLayout>(Resource.Id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
             LayoutInflater.Inflate(Resource.Layout.ad_detail, contentFrameLayout);
